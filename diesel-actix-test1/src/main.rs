bb2ca8b2f0b08e0b2b5090bd3f15c7d362af086d
ayoub <aybouras@student.1337.ma>
2024-04-10 23:42:07 +0100

    CRUD ops finished

--- diesel-actix-test1/src/main.rs
+++ diesel-actix-test1/src/main.rs
@@ -1,4 +1,3 @@
-// main.rs
 
 extern crate diesel;
 // use crate::diesel;
@@ -6,17 +5,8 @@ extern crate diesel;
 pub mod schema;
 pub mod models;
 pub mod db_conn;
-// use crate::database_con::db_connection;
-// use crate::db_conn::db_connection;
-// use crate::db_conn::hello_world;
 
-
-
-// use diesel::prelude::*;
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
-use diesel::pg::PgConnection;
-use diesel::r2d2::ConnectionManager;
-// use diesel::r2d2::{self, ConnectionManager};
+use actix_web::{App, HttpServer};
 use crate::schema::*;
 
 
@@ -24,7 +14,7 @@ use crate::schema::*;
 mod handlers {
     use crate::db_conn;
 
-    use crate::models::{NewUser, User};
+    use crate::models::User;
     use actix_web::{web, Responder, HttpResponse};
     use serde_json::json;
 
@@ -41,23 +31,10 @@ mod handlers {
             .load::<User>(&mut dbconn)
             .expect("Error loading users");
 
-        // println!("Displaying {} users", results.len());
-        // for user in results {
-        //     println!("{}", user.username);
-        //     println!("-----------\n");
-        //     println!("{}", user.email);
-        // }
-
-      
-
         web::Json(results)
 
         // HttpResponse::Ok().json(results)
     }
-
-    use crate::schema::users::dsl::id;
-    use crate::schema::users::dsl::username;
-    use crate::schema::users::dsl::email;
 
     pub async fn get_user_by_id(user_id: web::Path<i32>) -> HttpResponse {
         let user_id = user_id.into_inner();
@@ -80,127 +57,153 @@ mod handlers {
             }
             Err(_) => {
                 println!("Error loading user");
-                HttpResponse::InternalServerError().json(json!({"error": "Internal Server Error"}))
+                HttpResponse::NotFound().json(json!({"error": "UserId not found"}))
             }
         }
         
     }
 
 
-    // pub async fn get_user(pool: web::Data<r2d2::Pool<Connection<PgConnection>>>, user_id: web::Path<i32>) -> impl Responder {
-    //     let user = web::block(move || {
-    //         use crate::diesel::users::dsl::*;
+    //=========
+    pub async fn create_user(user_via_api: web::Json<User>) -> HttpResponse {
+        
 
-    //         users.find(user_id.into_inner()).first::<User>(&pool.get().unwrap())
-    //     })
-    //     .await
-    //     .unwrap();
+        // Extract the user from the request
+        let userr = user_via_api.into_inner();
 
-    //     web::Json(user)
-    // }
-
-    // pub async fn create_user(pool: web::Data<r2d2::Pool<Connection<PgConnection>>>, new_user: web::Json<crate::diesel::NewUser>) -> impl Responder {
-    //     let user = web::block(move || {
-    //         use crate::diesel::users::dsl::*;
-
-    //         diesel::insert_into(users).values(&new_user).get_result::<User>(&pool.get().unwrap())
-    //     })
-    //     .await
-    //     .unwrap();
-
-    //     web::Json(user)
-    // }
-
-    pub fn create_user() -> User {
-        // use crate::schema::users::dsl::*;
+        println!("new user: extracted {} {}", userr.username, userr.email);
+        // Example: Save the user to a database
+        let saved_user = save_user_to_database(userr).await;
+    
+        // Return an appropriate response, 201 Created, with the created user.
+        // HttpResponse::Created().json(saved_user)
+        match saved_user {
+            Ok(user) => {
+                HttpResponse::Created().json(user)
+            },
+            Err(_) => {
+                HttpResponse::InternalServerError().json(json!({"error": "Failed to save user, unique constraint violated"}))
+            }
+        }
+    }
+    
+    pub async fn save_user_to_database(user: User) -> Result<User, diesel::result::Error>
+    {
         use crate::schema::users;
 
+        // extract last saved id from db
         let mut dbconn = db_conn::db_connection().expect("Failed to get connection");
+        let last_id;
+        // last_id =  users
+        //     .select(id)
+        //     .order(id.desc())
+        //     .first::<i32>(&mut dbconn);
+            // .unwrap_or(0);
 
-        let name_ = "name1111".to_string();
-        let email_ = "email1111".to_string();
-    
-        let new_user = NewUser {
-            username: name_,
-            email: email_,
+            match users::table
+            .select(users::id)
+            .order(users::id.desc())
+            .first::<i32>(&mut dbconn) {
+                Ok(id_) => {
+                    last_id = id_ + 1;
+                },
+                Err(_) => {
+                    last_id = 0;
+                }
+            }
+
+        let saved_user = User {
+            id: last_id,
+            username: user.username.clone(),
+            email: user.email.clone(),
+            // Assign other user fields as needed
         };
-    
-        diesel::insert_into(users::table)
-            .values(&new_user)
-            .returning(User::as_returning())
-            .get_result(&mut dbconn)
-            .expect("Error saving new user")
-    }
 
-    pub async fn db_conn_test() -> impl Responder {
-        // "db connection established"      
-        match db_conn::db_connection() {
-            Ok(db_conn) => {
-                // Connection is established, perform any necessary operations
-                HttpResponse::Ok().body("Database connection established")
+        println!("Prepared user to be inserted: {:?}", saved_user);
+        
+        match diesel::insert_into(users::table)
+            .values(&saved_user)
+            .execute(&mut dbconn) {
+            Ok(_) => {
+                println!("User saved successfully: {:?}", saved_user);
             },
-            Err(_) => HttpResponse::InternalServerError().body("Failed to establish database connection"),
+            Err(_) => {
+                // println!("Error saving user");
+                return Err(diesel::result::Error::NotFound);
+            }
+            
+        }
+    
+        Ok(saved_user)
+    }
+    //====
+
+    pub async fn del_user_by_id(user_id: web::Path<i32>) -> HttpResponse {
+        let user_id = user_id.into_inner();
+        let mut dbconn = db_conn::db_connection().expect("Failed to get connection");
+    
+        let result = diesel::delete(users.find(user_id)).execute(&mut dbconn);
+    
+        match result {
+            Ok(rows_affected) if rows_affected > 0 => {
+                println!("User {} deleted successfully", user_id);
+                HttpResponse::Ok().json(json!({"message": "User deleted successfully"}))
+            },
+            Ok(_) => {
+                println!("User {} does not exist", user_id);
+                HttpResponse::NotFound().json(json!({"error": "User does not exist"}))
+            },
+            Err(_) => {
+                println!("Error deleting user");
+                HttpResponse::InternalServerError().json(json!({"error": "Internal server error"}))
+            }
         }
     }
 
-    // pub fn establish_connection() -> PgConnection {
-    //     dotenv().ok();
-    
-    //     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    //     PgConnection::establish(&database_url)
-    //         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-    // }
+    pub async fn hello_test() -> impl Responder
+    {
+        HttpResponse::Ok().body(
+            format!("Hello, the GET works")
+        )
+    }
+
+    pub async fn not_found() -> HttpResponse {
+        HttpResponse::NotFound().json(json!({
+            "error": "Not Found",
+            "message": "The requested route was not found"
+        }))
+    }
+
+
 }
 
 // Routes
 mod routes {
-    // use crate::handlers::{_create_user, _db_connection_established, _get_user, _get_users};
-    use crate::handlers::{db_conn_test, get_users, get_user_by_id};
+    use crate::handlers::{create_user, hello_test, get_user_by_id, get_users, del_user_by_id};
+
     use actix_web::web;
 
     pub fn configure(cfg: &mut web::ServiceConfig) {
+        cfg.route("/hello", web::get().to(hello_test));
         cfg.route("/users", web::get().to(get_users));
         cfg.route("/users/{user_id}", web::get().to(get_user_by_id));
-        // cfg.route("/users", web::post().to(create_user));
-        cfg.route("/db_con", web::get().to(db_conn_test));
+        cfg.route("/users", web::post().to(create_user));
+        cfg.route("/users/{user_id}", web::delete().to(del_user_by_id));
     }
 }
 
-// #[actix_web::main]
-// async fn main() -> std::io::Result<()> {
-
-//     // Start the HTTP server
-//     HttpServer::new(move || {
-//         let pool_clone = pool.clone();
-
-//         App::new()
-//             .data(pool_clone)
-//             .configure(routes::configure)
-//     })
-//     .bind("127.0.0.1:4444")?
-//     .run()
-//     .await
-// }
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    use actix_web::web;
 
     // Start the HTTP server
     HttpServer::new(move || {
 
         App::new()
             .configure(routes::configure)
+            .default_service(web::route().to(handlers::not_found))
     })
     .bind("127.0.0.1:4444")?
     .run()
     .await
 }
-
-//  fn main() {
-//     let new_user = handlers::create_user();
-//     println!("new user: created {} {}", new_user.username, new_user.email);
-//     println!("============ Fetching users=============");
-//     handlers::get_users();
-//     // handlers::test();
-//     println!("============ END=============");
-// }
--- diesel-actix-test1/src/models.rs
+++ diesel-actix-test1/src/models.rs
@@ -3,7 +3,7 @@ use diesel::{ Queryable, Insertable, Selectable };
 use crate::schema::users;
 use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Selectable, Serialize, Deserialize)]
+#[derive(Queryable, Selectable, Serialize, Deserialize, Debug, Insertable)]
 #[diesel(table_name = crate::schema::users)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct User {
@@ -12,7 +12,8 @@ pub struct User {
     pub email: String,
 }
 
-#[derive(Insertable)]
+// no longer needed with the api part
+#[derive(Insertable, Serialize, Deserialize, Debug)]
 #[diesel(table_name = users)]
 pub struct NewUser {
     pub username: String,
